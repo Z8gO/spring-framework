b7b68693a0e6d36f80b168c3772b4076c4839b9a
zhanghuang <zhanghuang@dcits.com>
2020-09-29 16:39:52 +0800

    fix

--- spring-framework-demo-test/src/test/java/my/test/demo/test/TestForDemo.java
+++ spring-framework-demo-test/src/test/java/my/test/demo/test/TestForDemo.java
@@ -1,6 +1,8 @@
 package my.test.demo.test;
 
+import my.test.demo.config.TestConfig;
 import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Created by zhanghuang on 2020/9/29
@@ -13,6 +15,10 @@ public class TestForDemo {
 
 	@Test
 	public  void  demo(){
-		System.out.println("===========Test=================");
+		AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+		my.test.demo.repository.Test test = acac.getBean(my.test.demo.repository.Test.class);
+
+		test.hello();
 	}
 }
